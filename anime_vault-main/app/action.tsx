38f2b2bc2f6792758e17472efdd5d5c2ad846b6c
Ravkeerat02 <ravkeerat.singh02@gmail.com>
2023-12-02 01:25:01 -0500

    complete

--- anime_vault-main/app/action.tsx
+++ anime_vault-main/app/action.tsx
@@ -1,6 +1,8 @@
 // executed on the server
 "use server";
 
+import AnimeCard, { AnimeProp } from "@/components/AnimeCard";
+
 // function declaration
 // fucntion param
 export const fetchAnime = async (page: number) => {
@@ -9,5 +11,7 @@ export const fetchAnime = async (page: number) => {
   );
   const data = await response.json();
 
-  return data;
+  return data.map((item: AnimeProp, index: number) => (
+    <AnimeCard key={item.id} anime={item} index={index} />
+  ));
 };
--- anime_vault-main/components/AnimeCard.tsx
+++ anime_vault-main/components/AnimeCard.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import { motion } from "framer-motion";
+import { MotionDiv } from "./MotionDiv";
 
 export interface AnimeProp {
   id: string;
@@ -17,9 +19,24 @@ interface Prop {
   index: number;
 }
 
-function AnimeCard({ anime }: Prop) {
+const variants = {
+  hidden: { opacity: 0, viewPort: { amount: 0 } },
+  visible: {
+    opacity: 1,
+    transition: { delay: 1, ease: "easeInOut", duration: 0.5 },
+    viewPort: { amount: 0 },
+  },
+};
+
+function AnimeCard({ anime, index }: Prop) {
   return (
-    <div className="max-w-sm rounded relative w-full">
+    <MotionDiv
+      initial="hidden"
+      animate="visible"
+      variants={variants}
+      transition={{ delay: index * 0.25, duration: 0.5, delay: 0.5 }}
+      className="max-w-sm rounded relative w-full"
+    >
       <div className="relative w-full h-[37vh]">
         <Image
           src={`https://shikimori.one${anime.image.original}`}
@@ -64,7 +81,7 @@ function AnimeCard({ anime }: Prop) {
           </div>
         </div>
       </div>
-    </div>
+    </MotionDiv>
   );
 }
 
