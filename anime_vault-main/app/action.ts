77560f27f96700a4ffc1f97ff9a26201e442b313
Ravkeerat02 <ravkeerat.singh02@gmail.com>
2023-12-02 00:57:22 -0500

    posters appear properly

--- anime_vault-main/app/action.ts
+++ anime_vault-main/app/action.ts
@@ -2,9 +2,12 @@
 "use server";
 
 // function declaration
-export const fetchAnime = async () => {
-  const response = await fetch("https://shikimori.one/api/animes");
+// fucntion param
+export const fetchAnime = async (page: number) => {
+  const response = await fetch(
+    `https://shikimori.one/api/animes?page=${page}&limit=5&order=popularity`
+  );
   const data = await response.json();
-  console.log(data);
+
   return data;
 };
